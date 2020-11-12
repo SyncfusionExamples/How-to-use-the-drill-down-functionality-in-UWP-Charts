e80b56ea6905d7ac7bd0b3c0b9f7940804eaf734
Hemalatha <hemalathavinai@gmail.com>
2020-11-12 13:56:45 +0530

    The sample included

---
+++ drilldown/ChartDemo/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Syncfusion.UI.Xaml.Charts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ChartDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Selection changed of chart 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void chart_SelectionChanged(object sender, ChartSelectionChangedEventArgs e)
+        {
+            IList items = e.SelectedSeries.ItemsSource as IList;
+            if (e.SelectedIndex != -1)
+            {
+                //Set the current window content from navigated page which is representing the chart with detailed
+                Window.Current.Content = new SecondPage(items[e.SelectedIndex] as Model);
+
+                //To get back to initial page, enable the back button with the help of SystemNavigationManager's AppViewBackButtonVisibility.
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+
+                //Once back button click to view the entire automobile list then, it will keep the content of window as main page
+                SystemNavigationManager.GetForCurrentView().BackRequested += (s, r) =>
+                {
+                    Window.Current.Content = new MainPage();
+                    SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+                };
+            }
+        }
+    }
+}
---
+++ drilldown/ChartDemo/ViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartDemo
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Model> Data { get; set; }
+        private ObservableCollection<Model> Data1 { get; set; }
+        private ObservableCollection<Model> Data2 { get; set; }
+        private ObservableCollection<Model> Data3 { get; set; }
+        private ObservableCollection<Model> Data4 { get; set; }
+
+        public ViewModel()
+        {
+            Data1 = new ObservableCollection<Model>
+            {
+                new Model("Toyota", 8),
+                new Model("Ford", 12),
+                new Model("GM", 17),
+                new Model("Renault", 6),
+                new Model("Fiat", 3),
+                new Model("Hyundai", 16)
+            };
+
+            Data2 = new ObservableCollection<Model>
+            {
+                new Model("Toyota", 7),
+                new Model("Chrysler", 12),
+                new Model("Nissan", 9),
+                new Model("Ford", 15),
+                new Model("Tata", 10),
+                new Model("Mahindra", 7)
+            };
+
+            Data3 = new ObservableCollection<Model>
+            {
+                new Model("Nissan", 9),
+                new Model("Chrysler", 4),
+                new Model("Ford", 7),
+                new Model("Toyota", 20),
+                new Model("Suzuki", 13),
+                new Model("Lada", 12),
+            };
+
+            Data4 = new ObservableCollection<Model>
+            {
+                new Model("Hummer", 11),
+                new Model("Ford", 5),
+                new Model("GM", 12),
+                new Model("Chrysler", 3),
+                new Model("Jaguar", 9),
+                new Model("Fiat", 8),
+            };
+
+            Data = new ObservableCollection<Model>
+            {
+                new Model("SUV", 25,Data1),
+                new Model("Car", 37,Data2),
+                new Model("Pickup", 15,Data3),
+                new Model("Minivan", 23,Data4)
+            };
+        }
+    }
+}
